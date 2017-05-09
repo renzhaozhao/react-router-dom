f0b9e147c6c7635b13e60ad1b9006ac829f1f2ee
renzhao <renzhao@vpgame.cn>
2017-05-09 16:03:45 +0800

    修改app

--- app.js
+++ app.js
@@ -3,7 +3,11 @@ const path = require('path')
 
 const app = express()
 
-app.use(express.static(path.join(__dirname, 'build')))
+app.use(express.static(path.resolve(__dirname, 'build')))
+
+app.get('*',function (req,res) {
+  res.sendFile(path.resolve(__dirname, 'build/index.html'))
+})
 
 app.set('port', process.env.PORT || 3000)
 app.listen(app.get('port'), () => {
